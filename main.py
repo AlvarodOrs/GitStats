759f077fd904ce8d3d659042a9b024e08e26a4c5
Álvaro d'Ors Nestares <alvarodorsnestares@gmail.com>
2025-12-20 16:45:36 +0100

    Multiple images creation and commit update

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ def main(card_format:int = 0, to_print:bool = False, call_api:bool = True, auto_
     else: data = load_config(f'data/{config["USERNAME"]}-stats.json')
 
     if to_print: write_json(data, config["USERNAME"], 4)
-    
-    if card_format == 0: svg_file = generate_default_stats_card(data)
-    if card_format == 1: svg_file = neutral.generate_stats_card(data)
-    if card_format == 2: svg_file = internship.generate_stats_card(data)
-    if card_format == 3: svg_file = oss.generate_stats_card(data)
-    if card_format == 4: svg_file = backend.generate_stats_card(data)
+    svg_file = []
+    if card_format == 0: svg_file.append(generate_default_stats_card(data))
+    card_format += 1
+    if card_format == 1: svg_file.append(neutral.generate_stats_card(data))
+    card_format += 1
+    if card_format == 2: svg_file.append(internship.generate_stats_card(data))
+    card_format += 1
+    if card_format == 3: svg_file.append(oss.generate_stats_card(data))
+    card_format += 1
+    if card_format == 4: svg_file.append(backend.generate_stats_card(data))
     
     if auto_commit:
-        success = auto_update_github(
-            file_paths=[svg_file],
-            commit_message="#GitStats card update#"
-        )
-                
-        if success: print("Successfully updated GitHub repository!")
-        else: print("Failed to update GitHub repository")
-    
+        for file in svg_file:
+            success = auto_update_github(
+                file_paths=[file],
+                commit_message="#GitStats card update#"
+            )
+            if success: print(f"Successfully updated GitHub repository with {file}!")
+            else: print("Failed to update GitHub repository")
+        
 if __name__ == '__main__':
     main(card_format=0, to_print=True, call_api=True, auto_commit=True)
