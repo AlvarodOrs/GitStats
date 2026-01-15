67b0ebf90c5c5667559be5e5bd00bdefe1357528
Álvaro d'Ors Nestares <alvarodorsnestares@gmail.com>
2026-01-15 16:22:37 +0100

    Minor tweaks

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 from utils.tools import load_config, write_json
 from workers import fetch_data
-from generators.models import backend, internship, neutral, oss
-from generators.generator import generate_stats_card as generate_default_stats_card
+from generators import render
 from utils.git_updater import auto_update_github
 
-def main(card_format:int = 0, to_print:bool = False, call_api:bool = True, auto_commit:bool = True):
+def _card_print(card_indx:int, data:dict) -> list[str]:
+    if card_indx == 0: svg_files = [render.generate_stats_card(data, card_model) for card_model in range(1, 6)]
+    
+    else: svg_files = [render.generate_stats_card(data, card_indx)]
+
+    return svg_files
+
+def _commit(to_commit:bool, svg_files:list[str]):
+    if not to_commit: return "No commit requested"
+    else:
+        if not isinstance(svg_files, list): svg_files = [svg_files]
+
+        success = auto_update_github(
+            file_paths=svg_files,
+            commit_message="#GitStats card update#"
+        )
+
+        return f"Successfully updated GitHub repository with {svg_files}!" if success else "Failed to update GitHub repository"
+
+def main(card_indx:int = 0, to_write:bool = False, call_api:bool = True, auto_commit:bool = True):
     config = load_config()
     
     if call_api: data = fetch_data.fetch_data(config)
     else: data = load_config(f'data/{config["USERNAME"]}-stats.json')
 
-    if to_print: write_json(data, config["USERNAME"], 4)
-    svg_file = []
-    if card_format == 0: svg_file.append(generate_default_stats_card(data))
-    # card_format += 1
-    if card_format == 1: svg_file.append(neutral.generate_stats_card(data))
-    # card_format += 1
-    if card_format == 2: svg_file.append(internship.generate_stats_card(data))
-    # card_format += 1
-    if card_format == 3: svg_file.append(oss.generate_stats_card(data))
-    # card_format += 1
-    if card_format == 4: svg_file.append(backend.generate_stats_card(data))
-    
-    if auto_commit:
-        for file in svg_file:
-            success = auto_update_github(
-                file_paths=[file],
-                commit_message="#GitStats card update#"
-            )
-            if success: print(f"Successfully updated GitHub repository with {file}!")
-            else: print("Failed to update GitHub repository")
+    if to_write: write_json(data, config["USERNAME"], 4)
+
+    svg_file = _card_print(card_indx, data)
+
+    if auto_commit: print(_commit(auto_commit, svg_file))
+
         
 if __name__ == '__main__':
-    main(card_format=0, to_print=True, call_api=True, auto_commit=True)
+    main(card_indx=0, to_write=True, call_api=True, auto_commit=True)
